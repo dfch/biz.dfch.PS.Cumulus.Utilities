89617008ef288c050656b453e8c327b19fccfa35
tgdriro5 <ronald.rink@d-fens.net>
2014-12-07 17:41:17 +0100

    2014-12-07; rrink; ADD: solve issue #3

---
+++ Remove-Entity.ps1
@@ -0,0 +1,431 @@
+function Remove-Entity {
+<#
+.SYNOPSIS
+Removes one or more entities from a Cumulus entity set.
+
+
+.DESCRIPTION
+Removes one or more entities from the Cumulus entity set.
+
+You can retrieve one ore more entities from the entity set by specifying 
+Id, Name or other properties.
+
+
+.INPUTS
+The Cmdlet can either remove entities by id or by object reference.
+See PARAMETERS section on possible inputs.
+
+
+.OUTPUTS
+default | json | json-pretty | xml | xml-pretty
+
+
+.EXAMPLE
+Remove-Entity $entity;
+
+Removes an entity.
+
+
+.EXAMPLE
+Remove-Entity -Id 42 -EntitySetName Command -Confirm;
+
+Removes entity with Id 42 from entity set 'Commands' in the default Services 
+reference. Operation must be confirmed interactively
+
+
+.EXAMPLE
+Remove-Entity -Id 42 -EntitySetName Command -Service ApplicationData;
+
+Removes entity with Id 42 from entity set 'Commands' in the 'ApplicationData' 
+Services reference.
+
+
+.EXAMPLE
+Remove-Entity $entity -Force;
+
+Remove entity '$entity' even if the entity is in a state other than 'Unchanged'
+
+
+.EXAMPLE
+$svc.ApplicationData.Commands |? Status -eq 'FAILED' | Remove-Entity -Confirm:$false;
+
+Retrieves all Commands with Status 'FAILED'.
+
+
+.EXAMPLE
+Remove-Entity -Id 1,2,3,4 -EntitySetName Commands -Confirm:$false;
+
+Retrieves all Command entities with Id 1,2,3,4.
+
+
+.LINK
+Online Version: http://dfch.biz/biz/dfch/PS/Cumulus/Utilities/Remove-Entity/
+
+
+.NOTES
+See module manifest for required software versions and dependencies.
+
+
+#>
+[CmdletBinding(
+    SupportsShouldProcess = $true
+	,
+    ConfirmImpact = "High"
+	,
+	HelpURI = 'http://dfch.biz/biz/dfch/PS/Cumulus/Utilities/Remove-Entity/'
+	,
+	DefaultParameterSetName = 'o'
+)]
+PARAM 
+(
+	# Specifies the id of the entity to remove
+	[Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'id')]
+	[ValidateRange(1, [int]::MaxValue)]
+	$Id
+	,
+	# Specifies the entity set name of the entity to remove
+	[Parameter(Mandatory = $true, Position = 1, ParameterSetName = 'id')]
+	[ValidateNotNullOrEmpty()]
+	[string] $EntitySetName
+	,
+	# Specifies the service or container name of the entity to remove
+	[ValidateScript( { if($svc.Keys -contains $_) { $true; } else { throw('{0}: Invalid service name specified.' -f $_); } } )]
+	[Parameter(Mandatory = $false, Position = 2, ParameterSetName = 'id')]
+	[Alias("Container")]
+	[string] $Service = 'ApplicationData'
+	,
+	# Specifies the entity to remove
+	[Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0, ParameterSetName = 'o')]
+	[Alias("Entity")]
+	$InputObject
+	,
+	# Specifies the entity to remove
+	[Parameter(Mandatory = $false)]
+	[switch] $Force
+	,
+	# Service reference to Cumulus
+	[Parameter(Mandatory = $false)]
+	[Alias("Services")]
+	[hashtable] $svc = (Get-Variable -Name $MyInvocation.MyCommand.Module.PrivateData.MODULEVAR -ValueOnly).Services
+	,
+	# Specifies the return format of the Cmdlet
+	[ValidateSet('default', 'json', 'json-pretty', 'xml', 'xml-pretty')]
+	[Parameter(Mandatory = $false)]
+	[alias("ReturnFormat")]
+	[string] $As = 'default'
+)
+
+BEGIN 
+{
+	$datBegin = [datetime]::Now;
+	[string] $fn = $MyInvocation.MyCommand.Name;
+	Log-Debug -fn $fn -msg ("CALL. svc '{0}'. Name '{1}'." -f ($svc -is [Object]), $Name) -fac 1;
+	
+	# Parameter validation
+	if($svc.ApplicationData -isnot [CumulusWrapper.ApplicationData.ApplicationData]) 
+	{
+		$msg = "svc: Parameter validation FAILED. Connect to the server before using the Cmdlet.";
+		Log-Error $fn $msg;
+		$e = New-CustomErrorRecord -m $msg -cat InvalidData -o $svc.ApplicationData;
+		$PSCmdlet.ThrowTerminatingError($e);
+	}
+	
+	if($PSCmdlet.ParameterSetName -eq 'id')
+	{
+		if(!(Get-Member -InputObject $svc.$Service -MemberType Properties -Name $EntitySetName)) 
+		{
+			$msg = "EntitySetName: Parameter validation FAILED. '{0}' is not a valid entity set in '{1}'." -f $EntitySetName, $Service;
+			Log-Error $fn $msg;
+			$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $EntitySetName;
+			$PSCmdlet.ThrowTerminatingError($e);
+		}
+		$InputObjectTemp = New-Object System.Collections.ArrayList($Id.Count);
+		foreach($Object in $Id)
+		{
+			$ObjectTemp = $svc.$Service.$EntitySetName.AddQueryOption('$filter', ('Id eq {0}' -f $Object)).AddQueryOption('$top', 1) | Select;
+			if(!$ObjectTemp)
+			{
+				$EntityToRemoveMsg = '{0}-{1}-{2}' -f $Service, $EntitySetName, $Object;
+				$msg = "Id: Parameter validation FAILED. '{0}' is not a valid entity." -f $EntityToRemoveMsg;
+				Log-Error $fn $msg;
+				$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $EntityToRemoveMsg;
+				$PSCmdlet.ThrowTerminatingError($e);
+			}
+			$null = $InputObjectTemp.Add($ObjectTemp);
+		}
+		$InputObject = $InputObjectTemp.ToArray();
+		Remove-Variable InputObjectTemp -ErrorAction:SilentlyContinue -Confirm:$false;
+		Remove-Variable ObjectTemp -ErrorAction:SilentlyContinue -Confirm:$false;
+	}
+	$r = @();
+}
+# BEGIN
+
+PROCESS 
+{
+
+# Default test variable for checking function response codes.
+[Boolean] $fReturn = $false;
+# Return values are always and only returned via OutputParameter.
+$OutputParameter = $null;
+
+try 
+{
+	# Parameter validation
+	# N/A
+	
+	foreach($Object in $InputObject)
+	{
+		$ObjectDescriptor = $null;
+		foreach($Service in $svc.Keys) 
+		{ 
+			$ObjectDescriptor = $svc.$Service.GetEntityDescriptor($Object); 
+			if($ObjectDescriptor)
+			{
+				break;
+			}
+		}
+		if(!$ObjectDescriptor -Or !$Service)
+		{
+			$msg = "Object: Parameter validation FAILED. Object is not a valid entity.`r`n{0}" -f ($Object | Out-String);
+			Log-Error $fn $msg;
+			$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $Object;
+			$PSCmdlet.ThrowTerminatingError($e);
+		}
+
+		$EntitySetName = 'UnknownEntitySetName';
+		if(([uri] $svc.$Service.GetEntityDescriptor($Object).Identity).Segments[-1] -match '^([^(]+)\(.+$')
+		{
+			$EntitySetName = $Matches[1];
+		}
+		$EntityToRemoveMsg = '{0}-{1}-{2}' -f $Service, $EntitySetName, $Object.Id;
+		if(!$PSCmdlet.ShouldProcess(($Object | Out-String)))
+		{
+			continue;
+		}
+		if( !$Force -And $ObjectDescriptor.State -ne [System.Data.Services.Client.EntityStates]::Unchanged )
+		{
+			$msg = "Objects with a status '{0}' can only be removed with the 'Force' parameter.`r`n{1}" -f $ObjectDescriptor.State, ($Object | Out-String);
+			Log-Error $fn $msg;
+			$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $Object;
+			$PSCmdlet.ThrowTerminatingError($e);
+		}
+		# Delete object
+		Log-Debug $fn ("Deleting entity '{0}' ..." -f $EntityToRemoveMsg);
+		try
+		{
+			$svc.$Service.DeleteObject($Object);
+			$Response = $svc.$Service.SaveChanges();
+		}
+		catch
+		{
+			Log-Error $fn ("Deleting entity '{0}' FAILED." -f $EntityToRemoveMsg);
+			throw($_);
+		}
+		Log-Info $fn ("Deleting entity '{0}' SUCCEEDED." -f $EntityToRemoveMsg);
+		$r += $Response;
+	}
+}
+catch 
+{
+	if($gotoSuccess -eq $_.Exception.Message) 
+	{
+		$fReturn = $true;
+	} 
+	else 
+	{
+		[string] $ErrorText = "catch [$($_.FullyQualifiedErrorId)]";
+		$ErrorText += (($_ | fl * -Force) | Out-String);
+		$ErrorText += (($_.Exception | fl * -Force) | Out-String);
+		$ErrorText += (Get-PSCallStack | Out-String);
+		
+		if($_.Exception -is [System.Net.WebException]) 
+		{
+			Log-Critical $fn ("[WebException] Request FAILED with Status '{0}'. [{1}]." -f $_.Execption.Status, $_);
+			Log-Debug $fn $ErrorText -fac 3;
+		}
+		else 
+		{
+			Log-Error $fn $ErrorText -fac 3;
+			if($gotoError -eq $_.Exception.Message) 
+			{
+				Log-Error $fn $e.Exception.Message;
+				$PSCmdlet.ThrowTerminatingError($e);
+			} 
+			elseif($gotoFailure -ne $_.Exception.Message) 
+			{ 
+				Write-Verbose ("$fn`n$ErrorText"); 
+			} 
+			else 
+			{
+				# N/A
+			}
+		}
+		$fReturn = $false;
+		$OutputParameter = $null;
+	}
+}
+finally 
+{
+	# Clean up
+	# N/A
+}
+
+}
+# PROCESS
+
+END 
+{
+	if(0 -eq $r.Count)
+	{
+		$r = $null;
+	}
+	elseif(1 -eq $r.Count)
+	{
+		$r = $r[0];
+	}
+	switch($As) 
+	{
+		'xml' { $OutputParameter = (ConvertTo-Xml -InputObject $r).OuterXml; }
+		'xml-pretty' { $OutputParameter = Format-Xml -String (ConvertTo-Xml -InputObject $r).OuterXml; }
+		'json' { $OutputParameter = ConvertTo-Json -InputObject $r -Compress; }
+		'json-pretty' { $OutputParameter = ConvertTo-Json -InputObject $r; }
+		Default { $OutputParameter = $r; }
+	}
+	$OutputParameter
+	$fReturn = $true;
+
+	$datEnd = [datetime]::Now;
+	Log-Debug -fn $fn -msg ("RET. fReturn: [{0}]. Execution time: [{1}]ms. Started: [{2}]." -f $fReturn, ($datEnd - $datBegin).TotalMilliseconds, $datBegin.ToString('yyyy-MM-dd HH:mm:ss.fffzzz')) -fac 2;
+
+}
+# END
+
+} # function
+
+if($MyInvocation.ScriptName) { Export-ModuleMember -Function Remove-Entity; } 
+
+# SIG # Begin signature block
+# MIIW3AYJKoZIhvcNAQcCoIIWzTCCFskCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUZGQrg57+WEQHjuye8ckxf2z2
+# amOgghGYMIIEFDCCAvygAwIBAgILBAAAAAABL07hUtcwDQYJKoZIhvcNAQEFBQAw
+# VzELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNV
+# BAsTB1Jvb3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw0xMTA0
+# MTMxMDAwMDBaFw0yODAxMjgxMjAwMDBaMFIxCzAJBgNVBAYTAkJFMRkwFwYDVQQK
+# ExBHbG9iYWxTaWduIG52LXNhMSgwJgYDVQQDEx9HbG9iYWxTaWduIFRpbWVzdGFt
+# cGluZyBDQSAtIEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlO9l
+# +LVXn6BTDTQG6wkft0cYasvwW+T/J6U00feJGr+esc0SQW5m1IGghYtkWkYvmaCN
+# d7HivFzdItdqZ9C76Mp03otPDbBS5ZBb60cO8eefnAuQZT4XljBFcm05oRc2yrmg
+# jBtPCBn2gTGtYRakYua0QJ7D/PuV9vu1LpWBmODvxevYAll4d/eq41JrUJEpxfz3
+# zZNl0mBhIvIG+zLdFlH6Dv2KMPAXCae78wSuq5DnbN96qfTvxGInX2+ZbTh0qhGL
+# 2t/HFEzphbLswn1KJo/nVrqm4M+SU4B09APsaLJgvIQgAIMboe60dAXBKY5i0Eex
+# +vBTzBj5Ljv5cH60JQIDAQABo4HlMIHiMA4GA1UdDwEB/wQEAwIBBjASBgNVHRMB
+# Af8ECDAGAQH/AgEAMB0GA1UdDgQWBBRG2D7/3OO+/4Pm9IWbsN1q1hSpwTBHBgNV
+# HSAEQDA+MDwGBFUdIAAwNDAyBggrBgEFBQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFs
+# c2lnbi5jb20vcmVwb3NpdG9yeS8wMwYDVR0fBCwwKjAooCagJIYiaHR0cDovL2Ny
+# bC5nbG9iYWxzaWduLm5ldC9yb290LmNybDAfBgNVHSMEGDAWgBRge2YaRQ2XyolQ
+# L30EzTSo//z9SzANBgkqhkiG9w0BAQUFAAOCAQEATl5WkB5GtNlJMfO7FzkoG8IW
+# 3f1B3AkFBJtvsqKa1pkuQJkAVbXqP6UgdtOGNNQXzFU6x4Lu76i6vNgGnxVQ380W
+# e1I6AtcZGv2v8Hhc4EvFGN86JB7arLipWAQCBzDbsBJe/jG+8ARI9PBw+DpeVoPP
+# PfsNvPTF7ZedudTbpSeE4zibi6c1hkQgpDttpGoLoYP9KOva7yj2zIhd+wo7AKvg
+# IeviLzVsD440RZfroveZMzV+y5qKu0VN5z+fwtmK+mWybsd+Zf/okuEsMaL3sCc2
+# SI8mbzvuTXYfecPlf5Y1vC0OzAGwjn//UYCAp5LUs0RGZIyHTxZjBzFLY7Df8zCC
+# BCgwggMQoAMCAQICCwQAAAAAAS9O4TVcMA0GCSqGSIb3DQEBBQUAMFcxCzAJBgNV
+# BAYTAkJFMRkwFwYDVQQKExBHbG9iYWxTaWduIG52LXNhMRAwDgYDVQQLEwdSb290
+# IENBMRswGQYDVQQDExJHbG9iYWxTaWduIFJvb3QgQ0EwHhcNMTEwNDEzMTAwMDAw
+# WhcNMTkwNDEzMTAwMDAwWjBRMQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFs
+# U2lnbiBudi1zYTEnMCUGA1UEAxMeR2xvYmFsU2lnbiBDb2RlU2lnbmluZyBDQSAt
+# IEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsk8U5xC+1yZyqzaX
+# 71O/QoReWNGKKPxDRm9+KERQC3VdANc8CkSeIGqk90VKN2Cjbj8S+m36tkbDaqO4
+# DCcoAlco0VD3YTlVuMPhJYZSPL8FHdezmviaJDFJ1aKp4tORqz48c+/2KfHINdAw
+# e39OkqUGj4fizvXBY2asGGkqwV67Wuhulf87gGKdmcfHL2bV/WIaglVaxvpAd47J
+# MDwb8PI1uGxZnP3p1sq0QB73BMrRZ6l046UIVNmDNTuOjCMMdbbehkqeGj4KUEk4
+# nNKokL+Y+siMKycRfir7zt6prjiTIvqm7PtcYXbDRNbMDH4vbQaAonRAu7cf9DvX
+# c1Qf8wIDAQABo4H6MIH3MA4GA1UdDwEB/wQEAwIBBjASBgNVHRMBAf8ECDAGAQH/
+# AgEAMB0GA1UdDgQWBBQIbti2nIq/7T7Xw3RdzIAfqC9QejBHBgNVHSAEQDA+MDwG
+# BFUdIAAwNDAyBggrBgEFBQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFsc2lnbi5jb20v
+# cmVwb3NpdG9yeS8wMwYDVR0fBCwwKjAooCagJIYiaHR0cDovL2NybC5nbG9iYWxz
+# aWduLm5ldC9yb290LmNybDATBgNVHSUEDDAKBggrBgEFBQcDAzAfBgNVHSMEGDAW
+# gBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0BAQUFAAOCAQEAIlzF3T30
+# C3DY4/XnxY4JAbuxljZcWgetx6hESVEleq4NpBk7kpzPuUImuztsl+fHzhFtaJHa
+# jW3xU01UOIxh88iCdmm+gTILMcNsyZ4gClgv8Ej+fkgHqtdDWJRzVAQxqXgNO4yw
+# cME9fte9LyrD4vWPDJDca6XIvmheXW34eNK+SZUeFXgIkfs0yL6Erbzgxt0Y2/PK
+# 8HvCFDwYuAO6lT4hHj9gaXp/agOejUr58CgsMIRe7CZyQrFty2TDEozWhEtnQXyx
+# Axd4CeOtqLaWLaR+gANPiPfBa1pGFc0sGYvYcJzlLUmIYHKopBlScENe2tZGA7Bo
+# DiTvSvYLJSTvJDCCBJ8wggOHoAMCAQICEhEhQFwfDtJYiCvlTYaGuhHqRTANBgkq
+# hkiG9w0BAQUFADBSMQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFsU2lnbiBu
+# di1zYTEoMCYGA1UEAxMfR2xvYmFsU2lnbiBUaW1lc3RhbXBpbmcgQ0EgLSBHMjAe
+# Fw0xMzA4MjMwMDAwMDBaFw0yNDA5MjMwMDAwMDBaMGAxCzAJBgNVBAYTAlNHMR8w
+# HQYDVQQKExZHTU8gR2xvYmFsU2lnbiBQdGUgTHRkMTAwLgYDVQQDEydHbG9iYWxT
+# aWduIFRTQSBmb3IgTVMgQXV0aGVudGljb2RlIC0gRzEwggEiMA0GCSqGSIb3DQEB
+# AQUAA4IBDwAwggEKAoIBAQCwF66i07YEMFYeWA+x7VWk1lTL2PZzOuxdXqsl/Tal
+# +oTDYUDFRrVZUjtCoi5fE2IQqVvmc9aSJbF9I+MGs4c6DkPw1wCJU6IRMVIobl1A
+# cjzyCXenSZKX1GyQoHan/bjcs53yB2AsT1iYAGvTFVTg+t3/gCxfGKaY/9Sr7KFF
+# WbIub2Jd4NkZrItXnKgmK9kXpRDSRwgacCwzi39ogCq1oV1r3Y0CAikDqnw3u7sp
+# Tj1Tk7Om+o/SWJMVTLktq4CjoyX7r/cIZLB6RA9cENdfYTeqTmvT0lMlnYJz+iz5
+# crCpGTkqUPqp0Dw6yuhb7/VfUfT5CtmXNd5qheYjBEKvAgMBAAGjggFfMIIBWzAO
+# BgNVHQ8BAf8EBAMCB4AwTAYDVR0gBEUwQzBBBgkrBgEEAaAyAR4wNDAyBggrBgEF
+# BQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFsc2lnbi5jb20vcmVwb3NpdG9yeS8wCQYD
+# VR0TBAIwADAWBgNVHSUBAf8EDDAKBggrBgEFBQcDCDBCBgNVHR8EOzA5MDegNaAz
+# hjFodHRwOi8vY3JsLmdsb2JhbHNpZ24uY29tL2dzL2dzdGltZXN0YW1waW5nZzIu
+# Y3JsMFQGCCsGAQUFBwEBBEgwRjBEBggrBgEFBQcwAoY4aHR0cDovL3NlY3VyZS5n
+# bG9iYWxzaWduLmNvbS9jYWNlcnQvZ3N0aW1lc3RhbXBpbmdnMi5jcnQwHQYDVR0O
+# BBYEFNSihEo4Whh/uk8wUL2d1XqH1gn3MB8GA1UdIwQYMBaAFEbYPv/c477/g+b0
+# hZuw3WrWFKnBMA0GCSqGSIb3DQEBBQUAA4IBAQACMRQuWFdkQYXorxJ1PIgcw17s
+# LOmhPPW6qlMdudEpY9xDZ4bUOdrexsn/vkWF9KTXwVHqGO5AWF7me8yiQSkTOMjq
+# IRaczpCmLvumytmU30Ad+QIYK772XU+f/5pI28UFCcqAzqD53EvDI+YDj7S0r1tx
+# KWGRGBprevL9DdHNfV6Y67pwXuX06kPeNT3FFIGK2z4QXrty+qGgk6sDHMFlPJET
+# iwRdK8S5FhvMVcUM6KvnQ8mygyilUxNHqzlkuRzqNDCxdgCVIfHUPaj9oAAy126Y
+# PKacOwuDvsu4uyomjFm4ua6vJqziNKLcIQ2BCzgT90Wj49vErKFtG7flYVzXMIIE
+# rTCCA5WgAwIBAgISESFgd9/aXcgt4FtCBtsrp6UyMA0GCSqGSIb3DQEBBQUAMFEx
+# CzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9iYWxTaWduIG52LXNhMScwJQYDVQQD
+# Ex5HbG9iYWxTaWduIENvZGVTaWduaW5nIENBIC0gRzIwHhcNMTIwNjA4MDcyNDEx
+# WhcNMTUwNzEyMTAzNDA0WjB6MQswCQYDVQQGEwJERTEbMBkGA1UECBMSU2NobGVz
+# d2lnLUhvbHN0ZWluMRAwDgYDVQQHEwdJdHplaG9lMR0wGwYDVQQKDBRkLWZlbnMg
+# R21iSCAmIENvLiBLRzEdMBsGA1UEAwwUZC1mZW5zIEdtYkggJiBDby4gS0cwggEi
+# MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDTG4okWyOURuYYwTbGGokj+lvB
+# go0dwNYJe7HZ9wrDUUB+MsPTTZL82O2INMHpQ8/QEMs87aalzHz2wtYN1dUIBUae
+# dV7TZVme4ycjCfi5rlL+p44/vhNVnd1IbF/pxu7yOwkAwn/iR+FWbfAyFoCThJYk
+# 9agPV0CzzFFBLcEtErPJIvrHq94tbRJTqH9sypQfrEToe5kBWkDYfid7U0rUkH/m
+# bff/Tv87fd0mJkCfOL6H7/qCiYF20R23Kyw7D2f2hy9zTcdgzKVSPw41WTsQtB3i
+# 05qwEZ3QCgunKfDSCtldL7HTdW+cfXQ2IHItN6zHpUAYxWwoyWLOcWcS69InAgMB
+# AAGjggFUMIIBUDAOBgNVHQ8BAf8EBAMCB4AwTAYDVR0gBEUwQzBBBgkrBgEEAaAy
+# ATIwNDAyBggrBgEFBQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFsc2lnbi5jb20vcmVw
+# b3NpdG9yeS8wCQYDVR0TBAIwADATBgNVHSUEDDAKBggrBgEFBQcDAzA+BgNVHR8E
+# NzA1MDOgMaAvhi1odHRwOi8vY3JsLmdsb2JhbHNpZ24uY29tL2dzL2dzY29kZXNp
+# Z25nMi5jcmwwUAYIKwYBBQUHAQEERDBCMEAGCCsGAQUFBzAChjRodHRwOi8vc2Vj
+# dXJlLmdsb2JhbHNpZ24uY29tL2NhY2VydC9nc2NvZGVzaWduZzIuY3J0MB0GA1Ud
+# DgQWBBTwJ4K6WNfB5ea1nIQDH5+tzfFAujAfBgNVHSMEGDAWgBQIbti2nIq/7T7X
+# w3RdzIAfqC9QejANBgkqhkiG9w0BAQUFAAOCAQEAB3ZotjKh87o7xxzmXjgiYxHl
+# +L9tmF9nuj/SSXfDEXmnhGzkl1fHREpyXSVgBHZAXqPKnlmAMAWj0+Tm5yATKvV6
+# 82HlCQi+nZjG3tIhuTUbLdu35bss50U44zNDqr+4wEPwzuFMUnYF2hFbYzxZMEAX
+# Vlnaj+CqtMF6P/SZNxFvaAgnEY1QvIXI2pYVz3RhD4VdDPmMFv0P9iQ+npC1pmNL
+# mCaG7zpffUFvZDuX6xUlzvOi0nrTo9M5F2w7LbWSzZXedam6DMG0nR1Xcx0qy9wY
+# nq4NsytwPbUy+apmZVSalSvldiNDAfmdKP0SCjyVwk92xgNxYFwITJuNQIto4zGC
+# BK4wggSqAgEBMGcwUTELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24g
+# bnYtc2ExJzAlBgNVBAMTHkdsb2JhbFNpZ24gQ29kZVNpZ25pbmcgQ0EgLSBHMgIS
+# ESFgd9/aXcgt4FtCBtsrp6UyMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQow
+# CKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcC
+# AQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBTFWNt4AwbqckKaTt+w
+# OW/h9RN5DjANBgkqhkiG9w0BAQEFAASCAQB4Xk41BdiAPcEpQc+3MU3VqbwQ/bn2
+# WYOi8dmevDFfsOHDwyvLBwrqyEF8r3KL4WlTMVakFuggWoiJ5ZHUgwENP46SkIuG
+# F/tLCywe1KoGPTOkSUWWHrup5NLaG+EOci4TICDxLybm5b6XehJZw6KiaGYJnsG2
+# zhYqYeOlzXctMfFSp8SQUKoeM4GGWnv8bMgNGdAf0RH+e0z6yiMxgXhcGVg8gGjK
+# IFuFoeW2GG/dUnx/bGBlS3Wuv3+fQeXactIlocmMu/kS3EHH6u2aYR1Wo/v9Z5g0
+# UkxJAb1iM/DYTB5Gby9WMaGcB7k4FRrKQ4QBiXdgchPVmrMUeqc5mWASoYICojCC
+# Ap4GCSqGSIb3DQEJBjGCAo8wggKLAgEBMGgwUjELMAkGA1UEBhMCQkUxGTAXBgNV
+# BAoTEEdsb2JhbFNpZ24gbnYtc2ExKDAmBgNVBAMTH0dsb2JhbFNpZ24gVGltZXN0
+# YW1waW5nIENBIC0gRzICEhEhQFwfDtJYiCvlTYaGuhHqRTAJBgUrDgMCGgUAoIH9
+# MBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE0MTIw
+# NzE2MzgwMFowIwYJKoZIhvcNAQkEMRYEFBHeiMcBdZK+PJQ9jh0i82LYAEfPMIGd
+# BgsqhkiG9w0BCRACDDGBjTCBijCBhzCBhAQUjOafUBLh0aj7OV4uMeK0K947NDsw
+# bDBWpFQwUjELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2Ex
+# KDAmBgNVBAMTH0dsb2JhbFNpZ24gVGltZXN0YW1waW5nIENBIC0gRzICEhEhQFwf
+# DtJYiCvlTYaGuhHqRTANBgkqhkiG9w0BAQEFAASCAQBVoVCpZNnMtiKCq33Lasd6
+# HbnPyZFqvXaHxkQWHXOaeT/0zRlfPgaUd8Vcn+EB9eiyDJQvx31+yWlHI056p5YO
+# t8PPbI2EqH2vq3nfSVCXLdydMHp8xeHX+2wz1US6iujwhvXpo3CWNGQlll7imqy7
+# kE9XmdmKGODQ8AgZHoVq8bHwYKGlOghx8Wi6A+VjOcQIwhPExq+eiskxBuM5Lmf3
+# pfPxghdABCcAzDAOy1SCeBdjzXaTzuq5ny/kHstmmrsM3JSfs5tBcHuaDAD9xTHk
+# U6gtnSYzCGQBi8bvGlReIbEQZ/AGkuZQW4iuNh0hyHA2IChipDFqTC3IEs3qLNe9
+# SIG # End signature block
