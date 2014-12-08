4c596ca8a9eb80f0b523af828f0188c5967306ec
tgdriro5 <ronald.rink@d-fens.net>
2014-12-08 15:58:35 +0100

    2012-12-08; rrink; CHG: update signature, nuget and minor typos

--- Set-IaasGroup.ps1
+++ Set-IaasGroup.ps1
@@ -1,123 +1,35 @@
 function Set-IaasGroup {
 <#
-
 .SYNOPSIS
-
 Creates or updates an IaasGroup.
-
 
 
 .DESCRIPTION
-
 Creates or updates an IaasGroup.
 
-
-
-.PARAMETER Name
-
-The name of the vCenter to process.
-
-
-
-.PARAMETER mu
-
-The ManagementUri object of the vCenter to process.
-
-
-
-.PARAMETER DatacenterName
-
-The name of the DataCenter to process.
-
-
-
-.PARAMETER ClusterName
-
-The name of the Cluster to process. This will be the name of the IaasGroup to create or update.
-
-
-
-.PARAMETER Site
-
-A site name for this IaasGroup.
-
-
-
-.PARAMETER Tiering
-
-A tiering level for this IaasGroup.
-
-
-
-.PARAMETER CreateIfNotExist
-
-Specify this switch to create a new IaasGroup if no existing is found.
-
-
-
-.PARAMETER Type
-
-The type of IaasGroup to create or update.
-
-
-
-.PARAMETER Version
-
-The version of the IaasGroup Parameters for the given Type.
-
-
-
-.PARAMETER ExcludedDatastores
-
-An array of regular expressions that will be matched against the datastore display names that should be skipped from import.
-
-
-
-.PARAMETER ExcludedNetworks
-
-An array of regular expressions that will be matched against the network display names that should be skipped from import.
-
-
-
-.PARAMETER svc
-
-A reference to the array of DataServiceContexts.
-
-
-
-.PARAMETER As
-
-Specify the return format of this Cmdlet (e.g. json or xml output).
-
+This Cmdlet is not a typical 'Set'-Cmdlet as it not just sets (or creates) 
+the contents of an IaasGroup 
 
 
 .EXAMPLE
-
 Create or update a vCenter type IaasGroup (default) with name "Cluster01" by specifying vCenter, Datastore, Cluster. All networks with name containing "local" will be skipped, as well as all datastores with either "vMotion", "Console", or "Mangement". Name of IaasGroup will be ABC and will have a Tiering level of GOLD.
 
 Set-IaasGroup -Name vCenter1 -Site ABC -Tiering GOLD -Datacenter "Datacenter01" -Cluster "Cluster01" -CreateIfNotExist -ExcludedNetworks @("vMotion","Management","Console") -ExcludedDatastores @("local")
 
 
-
 .EXAMPLE
-
 Similar as previous example but by specifying IaasGroup type and version explicitly. In addition there is not blacklist specified so all networks and datastores are possible for import. Cmdlet will fail if the IaasGroup does not already exist.
 
 Set-IaasGroup -Name vCenter1 -Site ABC -Tiering GOLD -Datacenter "Datacenter01" -Cluster "Cluster01" -Type vCenter -Version 3
 
 
-
 .LINK
-
 Online Version: http://dfch.biz/biz/dfch/PS/Cumulus/Utilities/Set-IaasGroup/
 
 
-
 .NOTES
+See module manifest for dependencies and requirements.
 
-Requires Powershell v3.
-
-Requires module 'biz.dfch.PS.System.Logging'.
 
 #>
 [CmdletBinding(
@@ -129,69 +41,86 @@ Requires module 'biz.dfch.PS.System.Logging'.
 	,
 	HelpURI='http://dfch.biz/biz/dfch/PS/Cumulus/Utilities/Set-IaasGroup/'
 )]
-PARAM (
+PARAM 
+(
+	# The name of the vCenter to process.
 	[Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'name')]
 	[string] $Name
 	,
+	# The ManagementUri object of the vCenter to process.
 	[Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'o')]
 	[Alias('ManagementUri')]
 	[CumulusWrapper.ApplicationData.ManagementUri] $mu
 	,
+	# The name of the DataCenter to process.
 	[Parameter(Mandatory = $true, Position = 1)]
 	[string] $DatacenterName
 	,
+	# The name of the Cluster to process. This will be the name of the IaasGroup to create or update.
 	[Parameter(Mandatory = $true, Position = 2)]
 	[string] $ClusterName
 	,
+	# A site name for this IaasGroup.
 	[Parameter(Mandatory = $true, Position = 3)]
 	[string] $Site
 	,
+	# A tiering level for this IaasGroup.
 	[Parameter(Mandatory = $true, Position = 4)]
 	[ValidateSet('GOLD', 'SILVER', 'BRONZE')]
 	[string] $Tiering
 	,
+	# Specifies the IaasGroup type to set or update
 	[Parameter(Mandatory = $false)]
 	[ValidateSet('vCenter')]
 	[string] $Type = 'vCenter'
 	,
+	# Specifies the IaasGroup version set or update
 	[Parameter(Mandatory = $false)]
 	[int] $Version = 1
 	,
+	# Specify this switch to create a new IaasGroup if no existing is found.
 	[Parameter(Mandatory = $false)]
 	[Alias("c")]
 	[switch] $CreateIfNotExist = $false
 	,
+	# An array of regular expressions that will be matched against the datastore display names that should be skipped from import.
 	[Parameter(Mandatory = $false)]
 	[string[]] $ExcludedDatastores
 	,
+	# An array of regular expressions that will be matched against the network display names that should be skipped from import.
 	[Parameter(Mandatory = $false)]
 	[string[]] $ExcludedNetworks
 	,
+	# A reference to the array of DataServiceContexts.
 	[Parameter(Mandatory = $false)]
 	[Alias("Services")]
 	[hashtable] $svc = (Get-Variable -Name $MyInvocation.MyCommand.Module.PrivateData.MODULEVAR -ValueOnly).Services
 	,
+	# Specify the return format of this Cmdlet (e.g. json or xml output).
 	[ValidateSet('default', 'json', 'json-pretty', 'xml', 'xml-pretty')]
 	[Parameter(Mandatory = $false)]
 	[alias("ReturnFormat")]
 	[string] $As = 'default'
-) # Param
+)
 
-BEGIN {
+BEGIN 
+{
+	$datBegin = [datetime]::Now;
+	[string] $fn = $MyInvocation.MyCommand.Name;
+	Log-Debug -fn $fn -msg ("CALL. svc '{0}'. Name '{1}'." -f ($svc -is [Object]), $Name) -fac 1;
+}
+# BEGIN
 
-$datBegin = [datetime]::Now;
-[string] $fn = $MyInvocation.MyCommand.Name;
-Log-Debug -fn $fn -msg ("CALL. svc '{0}'. Name '{1}'." -f ($svc -is [Object]), $Name) -fac 1;
-
-} # BEGIN
-PROCESS {
+PROCESS 
+{
 
 # Default test variable for checking function response codes.
 [Boolean] $fReturn = $false;
 # Return values are always and only returned via OutputParameter.
 $OutputParameter = $null;
 
-try {
+try 
+{
 
 	# Parameter validation
 	if($svc.ApplicationData -isnot [CumulusWrapper.ApplicationData.ApplicationData]) {
@@ -199,51 +128,60 @@ try {
 		Log-Error $fn $msg;
 		$e = New-CustomErrorRecord -m $msg -cat InvalidData -o $svc.ApplicationData;
 		throw($gotoError);
-	} # if
+	}
 	
-	if($PSCmdlet.ParameterSetName -eq 'name') {
+	if($PSCmdlet.ParameterSetName -eq 'name') 
+	{
 		$mu = $svc.ApplicationData.ManagementUris.AddQueryOption('$filter',("Name eq '{0}'" -f $Name)).AddQueryOption('$top',1) | Select;
-	} # if
-	if(!$mu) {
+	}
+	if(!$mu) 
+	{
 		$msg = "Name: Parameter validation FAILED. ManagementUri '{0}'." -f $Name;
 		Log-Error $fn $msg;
 		$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $Name;
 		throw($gotoError);
-	} # if
+	}
 	$muParameters = $mu.Value | ConvertFrom-Json;
 	
 	$ig = $svc.ApplicationData.IaasGroups.AddQueryOption('$filter',("Name eq '{0}'" -f $ClusterName)).AddQueryOption('$top',1) | Select;
-	if(!$ig) {
-		if(!$CreateIfNotExist) {
+	if(!$ig) 
+	{
+		if(!$CreateIfNotExist) 
+		{
 			$msg = "ClusterName: Parameter validation FAILED. IaasGroup '{0}' does not exist." -f $ClusterName;
 			Log-Error $fn $msg;
 			$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $ClusterName;
 			throw($gotoError);
-		} else {
+		} 
+		else 
+		{
 			$ig = New-Object CumulusWrapper.ApplicationData.IaasGroup;
-		} # if
-	} # if
+		}
+	}
 	
 	$igParameters = $ig.Parameters | ConvertFrom-Json -ErrorAction:SilentlyContinue;
-	if(!$igParameters) {
+	if(!$igParameters) 
+	{
 		$igParameters = @{};
 		$igParameters.Networks = @();
 		$igParameters.Datatores = @();
 	}
 	$DatacenterConfig = $muParameters.Datacenters.$DatacenterName;
-	if(!$DatacenterConfig) {
+	if(!$DatacenterConfig) 
+	{
 		$msg = "DatacenterName: Parameter validation FAILED. DatacenterName '{0}'." -f $DatacenterName;
 		Log-Error $fn $msg;
 		$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $Name;
 		throw($gotoError);
-	} # if
+	}
 	$ClusterConfig = $DatacenterConfig.Clusters.$ClusterName;
-	if(!$ClusterConfig) {
+	if(!$ClusterConfig) 
+	{
 		$msg = "ClusterName: Parameter validation FAILED. ClusterName '{0}'." -f $ClusterName;
 		Log-Error $fn $msg;
 		$e = New-CustomErrorRecord -m $msg -cat ObjectNotFound -o $Name;
 		throw($gotoError);
-	} # if
+	}
 	
 	$ig.Version = $Version;
 	$ig.Type = $Type;
@@ -260,87 +198,118 @@ try {
 	$params.Datastores = @();
 	$htds = @{};
 	$ClusterConfig.Datastores.psobject.properties | % { $htds[$_.Name] = $_.Value }
-	foreach($ds in $htds.GetEnumerator()) { 
+	foreach($ds in $htds.GetEnumerator()) 
+	{ 
 		$fSkip = $false;
-		foreach($excl in $ExcludedDatastores) {
-			if($ds.Value.Name -imatch $excl) {
+		foreach($excl in $ExcludedDatastores) 
+		{
+			if($ds.Value.Name -imatch $excl) 
+			{
 				Log-Warn $fn ("{0}: match found in ExcludedDatastores. Skipping import of this network ..." -f $ds.Value.Name);
 				$fSkip = $true;
 				break;
-			} # if
-		} # foreach
-		if($fSkip) { continue; }
+			}
+		}
+		if($fSkip) 
+		{ 
+			continue; 
+		}
 		$paramsds = $igParameters.Datastores |? DisplayName -eq $ds.Value.Name;
-		if(!$paramsds) { 
+		if(!$paramsds) 
+		{ 
 			$paramsds = @{}; 
 			$paramsds.DisplayName = $ds.Value.Name;
-		} # if
+		}
 		$paramsds.AvailableDiskGB = [Math]::Floor($ds.Value.FreeSpaceGB);
 		$paramsds.TotalDiskGB = [Math]::Floor($ds.Value.CapacityGB);
-		if($PSCmdlet.ShouldProcess(("vCenter '{0}'. DatacenterName '{1}'. ClusterName '{2}': set the following Datastore to ACTIVE ?`r`n{3}" -f $mu.Name, $DatacenterName, $ClusterName, ($paramsds | Out-String)))) {
+		if($PSCmdlet.ShouldProcess(("vCenter '{0}'. DatacenterName '{1}'. ClusterName '{2}': set the following Datastore to ACTIVE ?`r`n{3}" -f $mu.Name, $DatacenterName, $ClusterName, ($paramsds | Out-String)))) 
+		{
 			$paramsds.Active = $true;
-		} else {
+		} 
+		else 
+		{
 			$paramsds.Active = $false;
-		} # if
+		}
 		$params.Datastores += $paramsds;
-	} # foreach
+	}
 	
 	$params.Networks = @();
-	foreach($net in $ClusterConfig.Networks) { 
+	foreach($net in $ClusterConfig.Networks) 
+	{ 
 		$fSkip = $false;
-		foreach($excl in $ExcludedNetworks) {
-			if($net -imatch $excl) {
+		foreach($excl in $ExcludedNetworks) 
+		{
+			if($net -imatch $excl) 
+			{
 				Log-Warn $fn ("{0}: match found in ExcludedNetworks. Skipping import of this network ..." -f $net);
 				$fSkip = $true;
 				break;
-			} # if
-		} # foreach
-		if($fSkip) { continue; }
+			}
+		}
+		if($fSkip) { 
+			continue; 
+		}
 		$paramsnet = $igParameters.Networks |? DisplayName -eq $net;
-		if(!$paramsnet) {
+		if(!$paramsnet) 
+		{
 			$paramsnet = @{};
 			$paramsnet.DisplayName = $net;
-		} # if
+		}
 		$fReturn = $net -imatch 'VLAN\ *(\d\d\d?)';
-		if($fReturn) {
+		if($fReturn) 
+		{
 			$paramsnet.VlanId = $Matches[1];
-		} else {
+		} 
+		else 
+		{
 			$paramsnet.VlanId = 0;
-		} # if
-		if($PSCmdlet.ShouldProcess(("vCenter '{0}'. DatacenterName '{1}'. ClusterName '{2}': set the following Network to ACTIVE ?`r`n{3}" -f $mu.Name, $DatacenterName, $ClusterName, ($paramsnet | Out-String)))) {
+		}
+		if($PSCmdlet.ShouldProcess(("vCenter '{0}'. DatacenterName '{1}'. ClusterName '{2}': set the following Network to ACTIVE ?`r`n{3}" -f $mu.Name, $DatacenterName, $ClusterName, ($paramsnet | Out-String)))) 
+		{
 			$paramsnet.Active = $true;
-		} else {
+		} 
+		else 
+		{
 			$paramsnet.Active = $false;
-		} # if
+		}
 		$params.Networks += $paramsnet;
-	} # foreach
+	}
 
 	$ig.Parameters = $params | ConvertTo-Json;
-	if($PSCmdlet.ShouldProcess(("vCenter '{0}'. DatacenterName '{1}'. ClusterName '{2}': add or update the following IaasGroup information?`r`n{3}" -f $mu.Name, $DatacenterName, $ClusterName, ($params | ConvertTo-Json)))) {
-		if(!$ig.Id) {
+	if($PSCmdlet.ShouldProcess(("vCenter '{0}'. DatacenterName '{1}'. ClusterName '{2}': add or update the following IaasGroup information?`r`n{3}" -f $mu.Name, $DatacenterName, $ClusterName, ($params | ConvertTo-Json)))) 
+	{
+		if(!$ig.Id) 
+		{
 			$svc.ApplicationData.AddToIaasGroups($ig);
-		} # if
+		}
 		$svc.ApplicationData.UpdateObject($ig);
 		$r = $svc.ApplicationData.SaveChanges();
-	} else {
+	} 
+	else 
+	{
 		$null = $svc.ApplicationData.Detach($ig);
 		$r = $null;
-	} # if
+	}
 
-	switch($As) {
-	'xml' { $OutputParameter = (ConvertTo-Xml -InputObject $r).OuterXml; }
-	'xml-pretty' { $OutputParameter = Format-Xml -String (ConvertTo-Xml -InputObject $r).OuterXml; }
-	'json' { $OutputParameter = ConvertTo-Json -InputObject $r -Compress; }
-	'json-pretty' { $OutputParameter = ConvertTo-Json -InputObject $r; }
-	Default { $OutputParameter = $r; }
-	} # switch
+	switch($As) 
+	{
+		'xml' { $OutputParameter = (ConvertTo-Xml -InputObject $r).OuterXml; }
+		'xml-pretty' { $OutputParameter = Format-Xml -String (ConvertTo-Xml -InputObject $r).OuterXml; }
+		'json' { $OutputParameter = ConvertTo-Json -InputObject $r -Compress; }
+		'json-pretty' { $OutputParameter = ConvertTo-Json -InputObject $r; }
+		Default { $OutputParameter = $r; }
+	}
 	$fReturn = $true;
 
-} # try
-catch {
-	if($gotoSuccess -eq $_.Exception.Message) {
+}
+catch 
+{
+	if($gotoSuccess -eq $_.Exception.Message) 
+	{
 		$fReturn = $true;
-	} else {
+	} 
+	else 
+	{
 		[string] $ErrorText = "catch [$($_.FullyQualifiedErrorId)]";
 		$ErrorText += (($_ | fl * -Force) | Out-String);
 		$ErrorText += (($_.Exception | fl * -Force) | Out-String);
@@ -349,39 +318,50 @@ catch {
 		if($_.Exception -is [System.Net.WebException]) {
 			Log-Critical $fn ("[WebException] Request FAILED with Status '{0}'. [{1}]." -f $_.Status, $_);
 			Log-Debug $fn $ErrorText -fac 3;
-		} # [System.Net.WebException]
-		else {
+		}
+		else 
+		{
 			Log-Error $fn $ErrorText -fac 3;
-			if($gotoError -eq $_.Exception.Message) {
+			if($gotoError -eq $_.Exception.Message) 
+			{
 				Log-Error $fn $e.Exception.Message;
 				$PSCmdlet.ThrowTerminatingError($e);
-			} elseif($gotoFailure -ne $_.Exception.Message) { 
+			} 
+			elseif($gotoFailure -ne $_.Exception.Message) 
+			{ 
 				Write-Verbose ("$fn`n$ErrorText"); 
-			} else {
+			} 
+			else 
+			{
 				# N/A
-			} # if
-		} # other exceptions
+			}
+		}
 		$fReturn = $false;
 		$OutputParameter = $null;
-	} # !$gotoSuccess
-} # catch
-finally {
+	}
+}
+finally 
+{
 	# Clean up
 	# N/A
-} # finally
-
-} # PROCESS
-
-END {
-
-$datEnd = [datetime]::Now;
-Log-Debug -fn $fn -msg ("RET. fReturn: [{0}]. Execution time: [{1}]ms. Started: [{2}]." -f $fReturn, ($datEnd - $datBegin).TotalMilliseconds, $datBegin.ToString('yyyy-MM-dd HH:mm:ss.fffzzz')) -fac 2;
-
-# Return values are always and only returned via OutputParameter.
-return $OutputParameter;
-
-} # END
 }
+
+}
+# PROCESS
+
+END 
+{
+	$datEnd = [datetime]::Now;
+	Log-Debug -fn $fn -msg ("RET. fReturn: [{0}]. Execution time: [{1}]ms. Started: [{2}]." -f $fReturn, ($datEnd - $datBegin).TotalMilliseconds, $datBegin.ToString('yyyy-MM-dd HH:mm:ss.fffzzz')) -fac 2;
+
+	# Return values are always and only returned via OutputParameter.
+	return $OutputParameter;
+
+}
+# END
+
+} # function
+
 if($MyInvocation.ScriptName) { Export-ModuleMember -Function Set-IaasGroup; } 
 
 <#
@@ -393,8 +373,8 @@ if($MyInvocation.ScriptName) { Export-ModuleMember -Function Set-IaasGroup; }
 # SIG # Begin signature block
 # MIIW3AYJKoZIhvcNAQcCoIIWzTCCFskCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUN2yH0pxvMppsSBWHopeFx4oK
-# 3dygghGYMIIEFDCCAvygAwIBAgILBAAAAAABL07hUtcwDQYJKoZIhvcNAQEFBQAw
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUNDdUr5QYLXkfhJkWS3DVZXej
+# Ct+gghGYMIIEFDCCAvygAwIBAgILBAAAAAABL07hUtcwDQYJKoZIhvcNAQEFBQAw
 # VzELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNV
 # BAsTB1Jvb3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw0xMTA0
 # MTMxMDAwMDBaFw0yODAxMjgxMjAwMDBaMFIxCzAJBgNVBAYTAkJFMRkwFwYDVQQK
@@ -492,25 +472,25 @@ if($MyInvocation.ScriptName) { Export-ModuleMember -Function Set-IaasGroup; }
 # bnYtc2ExJzAlBgNVBAMTHkdsb2JhbFNpZ24gQ29kZVNpZ25pbmcgQ0EgLSBHMgIS
 # ESFgd9/aXcgt4FtCBtsrp6UyMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQow
 # CKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcC
-# AQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRXq+/0YcoQr1jcPAbc
-# a/UHkRxRjDANBgkqhkiG9w0BAQEFAASCAQCyMDOSRlyUAPEbV+86EICQvsJFPCna
-# ca1ZA10GrmzAYcs2DUb4o+sxD+pTz1h9xxbDTiyXsMtPU0r0/1hZwiKdqWZBuJx3
-# /yxHItFfN0HAzBdihrJmVPWoW5cKFV6zh9XEU/p+wQD2fpNRcTP3QbLOkeZZOP/C
-# 39wEFvj4tQ1cIUlNV8eT+L3XcaaZgVzI25VN4Q0gqfbh2+Qg16kg2b8UwB9H6cut
-# Ugckxzou1HaqEcpkSNHkL5YUNNiGDyPwJ8KIBGZ3TFl+YCB137g0zU2k8Wu61Tt3
-# 455xAjZkExTQ7y5K+6SP/E+3zAFcdmVvs5CHv6Z9oEL7XKBoluN3TFNnoYICojCC
+# AQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRHc2YtEe8zl5JJLe6i
+# HLMbYJRmTjANBgkqhkiG9w0BAQEFAASCAQCq7J1xJV4eGo4mmvM++qUAH2n412hE
+# lGkDw+yShBNOUoe0Xkf8q9l5/JwkVNtqf5pxHvCVyM2j6jnGezkRDcdr0dazwHyx
+# 9E8IZ63uifsJKM/Os0XHmaJfCLFQuF5DeUD+CrPqGJfRgxiMReRqR02PNdq9pDno
+# IxO9086QRYVdtDtW/B6JlW/QfXZM7lhkIQskrhZDW540GmBZENbGTWqt7xIn4cZy
+# KlG7IlHldIkv2/ITHRVKZ8Sd0C84EkxU4jU/YRJn11v/nSNrTVZ7mlVtUt3AJxj0
+# 8d7oaDHEJtev6v60cEX297edNu4TtBjY/JJp/J9cILUC6binyKnE18lFoYICojCC
 # Ap4GCSqGSIb3DQEJBjGCAo8wggKLAgEBMGgwUjELMAkGA1UEBhMCQkUxGTAXBgNV
 # BAoTEEdsb2JhbFNpZ24gbnYtc2ExKDAmBgNVBAMTH0dsb2JhbFNpZ24gVGltZXN0
 # YW1waW5nIENBIC0gRzICEhEhQFwfDtJYiCvlTYaGuhHqRTAJBgUrDgMCGgUAoIH9
 # MBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE0MTIw
-# NjA4MDI1NVowIwYJKoZIhvcNAQkEMRYEFK0jttM7WeHHB0U+710x1wYlq0ReMIGd
+# ODE0NTYxNVowIwYJKoZIhvcNAQkEMRYEFNnWrTPoRnw3azqdK4+7Gvd55JCzMIGd
 # BgsqhkiG9w0BCRACDDGBjTCBijCBhzCBhAQUjOafUBLh0aj7OV4uMeK0K947NDsw
 # bDBWpFQwUjELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2Ex
 # KDAmBgNVBAMTH0dsb2JhbFNpZ24gVGltZXN0YW1waW5nIENBIC0gRzICEhEhQFwf
-# DtJYiCvlTYaGuhHqRTANBgkqhkiG9w0BAQEFAASCAQB+hPLQkvGdlsrnC2czMgIT
-# XL52sdvLFEr+c7wBjp/WVnnGdh126HLPtE1qglaQs2RvcmowLCodwbpT/jSA5Ryf
-# b+YPcm2bzmmlPZhCtvCh2/RIjiDuA99ETkhYYgwHtZkx+qUJ3SsH7E8V09QxB1nW
-# wuCQvq19EWVpaEwIlNugi1TkEIzK0QU+ssn9ftqjzTDimwKsXTY12Kcun92t3YLp
-# /k3JovAIwW8x82XapxK7nvxOb+RhTPQljLzbRFagKaERBN0oqsJ+tv0VUMqXu2e1
-# X4S8y6HhhBPV/msMmtbvrAkGOm0gQIZL4VpG5fmYIUVspFzYgAEuxy0ec4lZtjet
+# DtJYiCvlTYaGuhHqRTANBgkqhkiG9w0BAQEFAASCAQA7MrV454tllvaMdu5/+1l4
+# S+rfsH82TeEt5V6XmPIw1RF3oEzvr59aMGKZJlLVqQjfvmjSbrPcGpcs8zxsrLxu
+# 2dDm0/cGzS63sDCaI/P7zTv++B9v1t+s4s5rdQ9A9JzR9a6FPdii72fo3FzinnGg
+# 4cylXSY0ykmQcUhtWF9zFMvetp+7wO2xIKHw8tvKENovI+Exj2dAQ5SZoUWBCiX/
+# gCA0DEAA5PHcw3jAqSzrMIRmXKedCOHJkoZCVpQ/b6DwVzLSAFtVF2iqTaw1C28y
+# r4XkYKXzzp3MpSWvLyGNMYcIMFajKj23P583pnwIigDqUZg0WSIMXwxWrbux7d+t
 # SIG # End signature block
