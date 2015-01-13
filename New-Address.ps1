a37b2f697eb14d63e9718f3a536d81559361192e
Ronald Rink <ronald.rink@d-fens.net>
2015-01-13 17:23:47 +0000

    2015-01-13; rrink; CHG: update to module version 1.0.4 for inclusion of New-Address Cmdlet 2015-01-13; rrink; ADD: started to add short license info in source files

--- New-Address.ps1
+++ New-Address.ps1
@@ -200,8 +200,27 @@ END
 if($MyInvocation.ScriptName) { Export-ModuleMember -Function New-Address; } 
 
 <#
+2015-01-13; rrink; ADD: support for VlanName.
+2015-01-13; rrink; DEL: $Reference parameter
+2015-01-13; rrink; ADD: $As additional OutputParameter (native address entity (now default) and OperationResponse)
 2014-12-11; rrink; ADD: New-Address.
 #>
+
+# 
+# Copyright 2014-2015 Ronald Rink, d-fens GmbH
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
 
 # SIG # Begin signature block
 # MIIW3AYJKoZIhvcNAQcCoIIWzTCCFskCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
